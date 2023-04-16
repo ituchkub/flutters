53fc4541b618fbf25271bf00d2334055728fb697
Kantathus Suwannarat <ituchkub@hotmail.com>
2023-04-16 21:57:44 +0700

    Add NikeShop

---
+++ Sneaker Shop/android/app/src/main/kotlin/ituch/sneakershop/sneakershop/MainActivity.kt
@@ -0,0 +1,6 @@
+package ituch.sneakershop.sneakershop
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ Sneaker Shop/lib/pages/home_page.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:sneakershop/conponents/bottom_nav_bar.dart';
+
+import 'cart_page.dart';
+import 'shop_page.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+  void navigateBottomBar(int newIndex) {
+    setState(() {
+      _selectedIndex = newIndex;
+    });
+  }
+
+  final List<Widget> _pages = [ShopPage(), CartPage()];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      bottomNavigationBar: MyBottomNavBar(
+        onTabChange: (index) => navigateBottomBar(index),
+      ),
+      body: _pages[_selectedIndex],
+    );
+  }
+}
